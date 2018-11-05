f0ab9f0f008476ccacfee622463cf209c40fc3e7
ameer666 <40859965+ameer666@users.noreply.github.com>
2018-11-05 15:04:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,9 @@ const client4 = new Discord.Client()
 const client5 = new Discord.Client()
 const client6 = new Discord.Client()
 const client7 = new Discord.Client()
+const client8 = new Discord.Client()
+const client9 = new Discord.Client()
+const client10 = new Discord.Client()
 var prefix1 = "1"
 var prefix2 = "2"
 var prefix3 = "3"
@@ -13,6 +16,9 @@ var prefix4 = "4"
 var prefix5 = "5"
 var prefix6 = "6"
 var prefix7 = "7"
+var prefix8 = "8"
+var prefix9 = "9"
+var prefix10 = "10"
 
 
 client1.on("ready", async() => {console.log(` _ ${client1.user.username} _ is Ready`)});
@@ -22,6 +28,9 @@ client4.on("ready", async() => {console.log(` _ ${client4.user.username} _ is Re
 client5.on("ready", async() => {console.log(` _ ${client5.user.username} _ is Ready`)});
 client6.on("ready", async() => {console.log(` _ ${client6.user.username} _ is Ready`)});
 client7.on("ready", async() => {console.log(` _ ${client7.user.username} _ is Ready`)});
+client8.on("ready", async() => {console.log(` _ ${client8.user.username} _ is Ready`)});
+client9.on("ready", async() => {console.log(` _ ${client9.user.username} _ is Ready`)});
+client10.on("ready", async() => {console.log(` _ ${client10.user.username} _ is Ready`)});
 
 
 
@@ -60,7 +69,21 @@ client7.on("message", message => {
         message.channel.send(`#daily`)
     }
 });
-
+client8.on("message", message => {
+    if(message.content === `${prefix8}d`) {
+        message.channel.send(`#daily`)
+    }
+});
+client9.on("message", message => {
+    if(message.content === `${prefix9}d`) {
+        message.channel.send(`#daily`)
+    }
+});
+client10.on("message", message => {
+    if(message.content === `${prefix10}d`) {
+        message.channel.send(`#daily`)
+    }
+});
 
 
 
@@ -100,7 +123,21 @@ client7.on("message", message => {
         message.channel.send(`#credits`)
     }
 });
-
+client8.on("message", message => {
+    if(message.content === `${prefix8}c`) {
+        message.channel.send(`#credits`)
+    }
+});
+client9.on("message", message => {
+    if(message.content === `${prefix9}c`) {
+        message.channel.send(`#credits`)
+    }
+});
+client10.on("message", message => {
+    if(message.content === `${prefix10}c`) {
+        message.channel.send(`#credits`)
+    }
+});
 
 
 
@@ -191,6 +228,43 @@ client7.on("message", message => {
     message.channel.send(args.join(" ")).catch(console.error);
   }
 });
+client8.on("message", message => {
+    if (!message.content.startsWith(prefix8)) return;
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix8.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command === "say") {
+          message.delete()
+    message.channel.send(args.join(" ")).catch(console.error);
+  }
+});
+client9.on("message", message => {
+    if (!message.content.startsWith(prefix9)) return;
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix9.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command === "say") {
+          message.delete()
+    message.channel.send(args.join(" ")).catch(console.error);
+  }
+});
+client10.on("message", message => {
+    if (!message.content.startsWith(prefix10)) return;
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix10.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command === "say") {
+          message.delete()
+    message.channel.send(args.join(" ")).catch(console.error);
+  }
+});
+
 
 
 
@@ -203,3 +277,6 @@ client4.login(process.env.BOT_TOKEN4)
 client5.login(process.env.BOT_TOKEN5)
 client6.login(process.env.BOT_TOKEN6)
 client7.login(process.env.BOT_TOKEN7)
+client8.login(process.env.BOT_TOKEN8)
+client9.login(process.env.BOT_TOKEN9)
+client10.login(process.env.BOT_TOKEN10)
